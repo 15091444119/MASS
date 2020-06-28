39ec50a4221789cfd587d1b7b4b91029a0b395c7
ZHAO <30514715+15091444119@users.noreply.github.com>
2020-06-28 19:01:30 +0800

    fix bug in test attention

--- MASS-unsupNMT/src/evaluation/eval_attention/test.py
+++ MASS-unsupNMT/src/evaluation/eval_attention/test.py
@@ -2,7 +2,7 @@ from .eval_attention import draw_attention
 import numpy as np
 
 def test_draw_attention():
-    draw_attention(np.array([[0.1, 0.5, 0.4],[0.5,0.5, 0]]), ["tgt_1", "tgt_2"], ["src_1", "src_2", "src_3"], "./tmp.jpg")
+    draw_attention(np.array([[0.1, 0.5, 0.4],[0.5,0.5, 0]]), target_tokens=["tgt_1", "tgt_2"], source_tokens=["src_1", "src_2", "src_3"], output_path="./tmp.jpg")
 
 if __name__ == "__main__":
     test_draw_attention()
