e156185cf351a7f05ebef008f302788c2c24eb7f
zhouzh <358400467@qq.com>
2020-08-04 11:33:50 +0000

    add scripts

--- MASS-unsupNMT/scripts/eval_attention.sh
+++ MASS-unsupNMT/scripts/eval_attention.sh
@@ -1,7 +1,11 @@
 export CUDA_VISIBLE_DEVICES="0"
+FantiFinetuned="/home/data_ti4_c/zhouzh/low-resource-mt/MASS/MASS-unsupNMT/dumped/en-zh-500w-ft-jointbpe-jointvocab/31b14y3hsf/59.pth" 
+FantiPretrained="/home/data_ti4_c/zhouzh/low-resource-mt/MASS/MASS-unsupNMT/dumped/en-zh-500w-pretrain-jointbpe-jointvocab/9pvn3bwu2s/checkpoint.pth" 
+JiantiSupervised="/home/data_ti4_c/zhouzh/low-resource-mt/MASS/MASS-unsupNMT/dumped/cn-en-zh-supervised/u7s91zmjk2/checkpoint.pth"
+JiantiPretrained="/home/data_ti4_c/zhouzh/low-resource-mt/MASS/MASS-unsupNMT/dumped/cn-en-zh-500w-pretrain-jointbpe-jointvocab/1jnrqg51bz/checkpoint.pth"
+JiantiFinetuned="/home/data_ti4_c/zhouzh/low-resource-mt/MASS/MASS-unsupNMT/dumped/cn-en-zh-500w-ft-jointbpe-jointvocab/q6vn71z093/checkpoint.pth"
 python3 -m src.evaluation.eval_attention.eval_attention \
 	--exp_name "eval_attention" \
-	--model_path "/home/data_ti4_c/zhouzh/low-resource-mt/MASS/MASS-unsupNMT/dumped/en-zh-500w-ft-jointbpe-jointvocab/31b14y3hsf/59.pth" \
+	--model_path $JiantiPretrained \
 	--src_lang "zh" \
 	--tgt_lang "en" \
-	--src_text "./test.txt"
--- MASS-unsupNMT/scripts/eval_context_word_translation/run_zh_en.sh
+++ MASS-unsupNMT/scripts/eval_context_word_translation/run_zh_en.sh
@@ -2,11 +2,13 @@ DataPath="/home/zhouzh/data/tokenized_data/para/zh-en"
 Src=$DataPath/valid.en-zh.zh
 Tgt=$DataPath/valid.en-zh.en
 BpeSrc="/home/data_ti4_c/zhouzh/low-resource-mt/XLM_MASS_preprocessed_data/pretrain/cn-split-sen-zh-en-pretrain/valid.en-zh.zh"
+BpeHyp="/home/data_ti4_c/zhouzh/low-resource-mt/XLM_MASS_preprocessed_data/pretrain/cn-split-sen-zh-en-pretrain/valid.en-zh.en"
 Hyp="/home/data_ti4_c/zhouzh/low-resource-mt/MASS/MASS-unsupNMT/dumped/cn-en-zh-supervised/u7s91zmjk2/hypotheses/hyp30.zh-en.valid.txt"
 Alignment="/home/data_ti4_c/zhouzh/low-resource-mt/MASS/MASS-unsupNMT/alignments/zh-en/final_align.gdfa.valid"
 python3 -m src.evaluation.eval_word_translation.alignment \
 	--src $Src \
 	--tgt $Tgt \
 	--bped_src $BpeSrc \
+        --bped_hyps $BpeHyp \
 	--hyp $Hyp \
  	--alignments $Alignment
