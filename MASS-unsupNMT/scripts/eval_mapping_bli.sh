55635dcb865edf5f086aae4d06c319bac350459b
zhouzh <zhouzh1995@gmai.com>
2020-09-08 09:30:01 +0800

    add save path in eval bli

---
+++ MASS-unsupNMT/scripts/eval_mapping_bli.sh
@@ -0,0 +1,9 @@
+python3 -m src.evaluation.eval_represetation.eval_bli \
+    --method "map" \
+    --dict \
+    --save_path \
+    --src_embs \
+    --tgt_embs \
+    --using_vocab \
+    --src_vocab \
+    --tgt_vocab
--- MASS-unsupNMT/src/evaluation/eval_representation/eval_bli.py
+++ MASS-unsupNMT/src/evaluation/eval_representation/eval_bli.py
@@ -214,6 +214,7 @@ if __name__ == "__main__":
     parser.add_argument("--preprocess", default="ucu")
     parser.add_argument("--metric", default="nn")
     parser.add_argument("--dict", help="bilingual dictionary")
+    parser.add_argument("--save_path", type=str, default=None, help="path to save bli result")
     if parser.parse_known_args()[0].method == "xlm":
         parser.add_argument("--reload", help="xlm model path")
         parser.add_argument("--model_name", type=str, choices=["model", "encoder", "decoder"], help='reload a xlm encoder or a xlm translation model')
@@ -250,6 +251,7 @@ if __name__ == "__main__":
             src_word2id=src_word2id,
             tgt_id2word=tgt_id2word,
             tgt_word2id=tgt_word2id,
-            dic=dic
+            dic=dic,
+            save_path=args.save_path
         )
         print("Scores: {}".format(scores))
