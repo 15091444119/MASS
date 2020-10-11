40302c9dd6f368c19cbfd21f889221f92fb8340f
zhouzh <zhouzh1995@gmai.com>
2020-10-11 11:03:25 +0800

    fix scripts

--- MASS-unsupNMT/scripts/train_combiner.sh
+++ MASS-unsupNMT/scripts/train_combiner.sh
@@ -1,11 +1,11 @@
-export CUDA_VISIBLE_DEVICES="2"
+export CUDA_VISIBLE_DEVICES="0"
 MODEL=/home/data_ti5_d/zhouzh/low-resource-mt/MASS/MASS-unsupNMT/dumped/cn-en-zh-500w-wwm-reload/j03q6ubj61/periodic-50.pth
 #/home/data_ti5_d/zhouzh/low-resource-mt/MASS/MASS-unsupNMT/dumped/cn-en-zh-500w-ft-jointbpe-jointvocab/q6vn71z093/checkpoint.pth
 #/home/data_ti5_d/zhouzh/low-resource-mt/MASS/MASS-unsupNMT/dumped/cn-en-zh-500w-checkpoint-pretrain/x363q5pus9/periodic-150.pth
 
 python train_combiner.py \
-	--exp_name debug                             \
-	--data_path ./combiner_data/debug_data \
+	--exp_name bpe-gru-average                             \
+	--data_path ./combiner_data \
 	--lgs 'zh-en'                                        \
 	--encoder_only False                                 \
 	--reload_model "$MODEL,$MODEL" \
@@ -17,7 +17,7 @@ python train_combiner.py \
 	--gelu_activation true                               \
 	--batch_size 100 \
 	--optimizer adam_inverse_sqrt,beta1=0.9,beta2=0.98,lr=0.0001 \
-	--epoch_size 5000                                  \
+	--epoch_size 100000                                  \
 	--max_epoch 200                                      \
 	--src_bped_words_path ./word_vocab/zh.vocab.bpe \
 	--tgt_bped_words_path ./word_vocab/en.vocab.bpe \
@@ -32,8 +32,8 @@ python train_combiner.py \
   --n_combiner_layers 4 \
   --validation_metrics _valid-average-loss \
   --bli_preprocess_method 'u' \
-  --splitter "CHAR" \
+  --splitter "BPE" \
   --origin_context_extractor "before_eos" \
-  --combiner_context_extractor "last_time"
+  --combiner_context_extractor "average"
 #  --reload_encoder_combiner_path /home/data_ti5_d/zhouzh/low-resource-mt/subword-combiner/MASS-unsupNMT/dumped/separate_combiner_gru_cos_4_layer/xt9davossf/best-valid-average-loss.pth \
 #  --eval_only True
